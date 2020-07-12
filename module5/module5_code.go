784da25e315da276720a859fb137dfbc002e91fc
Marcus Osborn <modevops@Marcuss-MacBook-Pro.local>
2020-07-12 15:46:37 -0600

    module4

--- module5/module5_code.go
+++ module5/module5_code.go
@@ -1,13 +1,14 @@
 package module5
 
-// GetExampleDotCom uses the "net/http" package to send a GET request to example.com
-/*
-func GetExampleDotCom() {
-	resp, err := http.Get("http://example.com/")
+import (
+	"fmt"
+	"net/http"
+)
+
+func GetExampleDotCOm() {
+	resp, err := http.Get("http://example.com")
 	if err != nil {
 		fmt.Println("something went wrong")
 	}
-
 	defer resp.Body.Close()
 }
-*/
